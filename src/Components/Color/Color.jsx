e2021ad516047277be05d7f8d6cc743397d4d2a2
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-09-02 13:30:29 +0200

    Add: delete color

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,13 @@
-export default function Color({ color }) {
+export default function Color({
+  color,
+  deleteColorID,
+  onAskDelete,
+  onCancelDelete,
+  onConfirmDelete,
+}) {
   // console.log("Find Issues 1");
+
+  const isDeleted = deleteColorID === color.id;
 
   return (
     <div
@@ -12,6 +20,18 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+
+      {isDeleted ? (
+        <>
+          <p>Are you sure ?</p>
+          <button onClick={onCancelDelete}>Cancel</button>
+          <button onClick={onConfirmDelete}>Delete</button>
+        </>
+      ) : (
+        <button type="button" onClick={onAskDelete}>
+          DELETE
+        </button>
+      )}
     </div>
   );
 }
